5211e9326931f8079c005bea2e794d1e3bb09983
ndahprtw <indhcnch@gmail.com>
2026-07-06 09:27:48 +0700

    [feat] queue email notifications

--- app/Notifications/BookAvailableNotification.php
+++ app/Notifications/BookAvailableNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class BookAvailableNotification extends Notification
+class BookAvailableNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
